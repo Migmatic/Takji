90dae74883e5654ab21836a66cd27fc05411233a
migmatic <lamandealexis@gmail.com>
2023-05-26 04:04:11 +0200

    Library

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', (event) => {
+document.addEventListener('DOMContentLoaded', () => {
     var mySwiper = new Swiper('.swiper-container', {
         loop: true,
         speed: 1000,
@@ -14,15 +14,138 @@ document.addEventListener('DOMContentLoaded', (event) => {
             stretch: 200,
             depth: 50,
             modifier: 1,
-            scale:0.85,
+            scale: 0.85,
             slideShadows: false,
         },
         pagination: {
             el: '.swiper-pagination',
             type: 'bullets',
-            clickable:'true',
+            clickable: 'true',
         },
+
+    })
+
+    const books = storeBooks()
+    const converter = converterTags()
+    const library = document.getElementsByClassName("wrapper-library")[0];
+    books.forEach(book => {
+        const single = document.createElement('li')
+        single.appendChild(
+            Object.assign(
+                document.createElement('div'),
+                {className: 'library-image'}
+            )
+        ).appendChild(
+            Object.assign(
+                document.createElement('img'),
+                {
+                    src: book.cover,
+                    alt: book.title
+                }
+            )
+        )
+        single.appendChild(
+            Object.assign(
+                document.createElement('h6'),
+                {innerHTML: book.title}
+            )
+        )
+
+        let tags_container = Object.assign(
+            document.createElement('div'),
+            {className: 'tags-library'}
+        )
+
+        book.tags.forEach(tag => {
+            tags_container.appendChild(
+                Object.assign(
+                    document.createElement('div'),
+                    {
+                        className: tag + " tag",
+                        innerHTML: converter[tag]
+                    }
+                )
+            )
+        })
+
+        single.appendChild(tags_container)
+
+        library.appendChild(single)
 
     })
 })
 
+
+function storeBooks() {
+    return [
+        {
+            title: "Volcanic Age",
+            cover: "image/books/volcanic.png",
+            tags: [
+                "aventure",
+                "action",
+                "art_martiaux"
+            ]
+        },
+
+        {
+            title: "Solo Leveling",
+            cover: "image/books/solo.png",
+            tags: [
+                "aventure",
+                "action",
+                "donjon"
+            ]
+        },
+
+        {
+            title: "The Archmage Returns After 4000 Years",
+            cover: "image/books/archmage.png",
+            tags: [
+                "aventure",
+                "action",
+                "fantaisie"
+            ]
+        },
+
+        {
+            title: "Lecteur Omniscient",
+            cover: "image/books/lecteur.png",
+            tags: [
+                "aventure",
+                "action",
+                "donjon"
+            ]
+        },
+
+        {
+            title: "Legend of the Northern Blade",
+            cover: "image/books/legend.png",
+            tags: [
+                "aventure",
+                "action",
+                "art_martiaux"
+            ]
+        },
+
+        {
+            title: "Return of the Frozen Player",
+            cover: "image/books/frozen.png",
+            tags: [
+                "aventure",
+                "action",
+                "donjon"
+            ]
+        }
+    ]
+}
+
+function converterTags() {
+    return {
+        aventure: "Aventure",
+        action: "Action",
+        art_martiaux: "Art Martiaux",
+        donjon: "Donjon",
+        fantaisie: "Fantaisie",
+    }
+}
