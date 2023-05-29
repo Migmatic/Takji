d42cf098f916a70d4b3c1f37c815b2dfce6e3d60
Hugo Briand <hugo.briand@crea-inseec.com>
2023-05-29 16:14:17 +0200

    responsive + nav

--- js/script.js
+++ js/script.js
@@ -180,3 +180,14 @@ function converterTags() {
         fantaisie: "Fantaisie",
     }
 }
+document.addEventListener('DOMContentLoaded', function() {
+    var hamburgerMenu = document.querySelector('.hamburger-menu');
+    var menu = document.querySelector('.menu');
+  
+    hamburgerMenu.addEventListener('click', function() {
+      menu.classList.toggle('show');
+    });
+  });
+  
+  
+
